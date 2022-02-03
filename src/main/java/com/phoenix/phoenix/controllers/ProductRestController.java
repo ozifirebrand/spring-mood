3ed60c598fccfc461f0afce19d26ee1fb2536584
Ozioma <oziomaokoroafor@gmail.com>
2022-02-03 17:16:22 +0100

    controller save product

--- src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
+++ src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
@@ -3,6 +3,7 @@ package com.phoenix.phoenix.controllers;
 import com.phoenix.phoenix.data.dto.ProductDto;
 import com.phoenix.phoenix.data.models.Product;
 import com.phoenix.phoenix.service.product.ProductService;
+import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
 import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,6 +31,12 @@ public class ProductRestController {
     @PostMapping
     public ResponseEntity<?> createProduct(ProductDto productDto){
 
-        return null;
+        try{
+            Product savedProduct = productService.createProduct(productDto);
+            return ResponseEntity.ok().body(savedProduct);
+        }
+        catch (BusinessLogicException exception){
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/phoenix/phoenix/web/exceptions/BusinessLogicException.java
@@ -0,0 +1,2 @@
+package com.phoenix.phoenix.web.exceptions;public class BusinessLogic {
+}
