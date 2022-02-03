f4618b393ed6c691a7f178882d652b0f52b615f2
Ozioma <oziomaokoroafor@gmail.com>
2022-02-03 15:53:48 +0100

    Product service implementation

--- src/main/java/com/phoenix/phoenix/service/product/ProductServiceImpl.java
+++ src/main/java/com/phoenix/phoenix/service/product/ProductServiceImpl.java
@@ -1,2 +1,38 @@
-package com.phoenix.phoenix.service.product;public class ProductServiceImpl {
+package com.phoenix.phoenix.service.product;
+
+import com.phoenix.phoenix.data.dto.ProductDto;
+import com.phoenix.phoenix.data.models.Product;
+import com.phoenix.phoenix.data.repository.ProductRepository;
+import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product findProductById(Long id) throws ProductDoesNotExistException {
+        if ( id == null ){
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if ( optionalProduct.isPresent()) return optionalProduct.get();
+
+        throw new ProductDoesNotExistException("Product with id: "+id+" does not exist");
+    }
+
+    @Override
+    public Product createProduct(ProductDto productDto) {
+        return null;
+    }
 }
