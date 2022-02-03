6dab25c8474cb1eda0a68af17cd77ab1bb5f0530
Ozioma <oziomaokoroafor@gmail.com>
2022-02-03 10:20:51 +0100

    Phoenix tests commit

--- src/test/java/com/phoenix/phoenix/PhoenixApplicationTests.java
+++ src/test/java/com/phoenix/phoenix/PhoenixApplicationTests.java
@@ -1,9 +1,14 @@
 package com.phoenix.phoenix;
 
 import lombok.extern.slf4j.Slf4j;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -13,6 +18,10 @@ class PhoenixApplicationTests {
 
 	@Value("${test.property.name}")
 	private String testName;
+
+	@Autowired
+	private DataSource datasource;
+
 	@Test
 	void contextLoads() {
 	}
@@ -21,6 +30,20 @@ class PhoenixApplicationTests {
 	void valueExists(){
 		assertThat(testName).isEqualTo("phoenix");
 		log.info(testName);
+	}
+
+	@Test
+	public void applicationCanConnectToDatabaseTest(){
+		assertThat(datasource).isNotNull();
+		Connection connection;
+		try {
+			connection = datasource.getConnection();
+			assertThat(connection).isNotNull();
+			log.info("Connection-->{}", connection.getSchema());
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
 	}
 
 }
