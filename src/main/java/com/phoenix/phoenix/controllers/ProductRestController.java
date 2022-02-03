2994c720931d58197661f4d26daf8475a4f58b69
Ozioma <oziomaokoroafor@gmail.com>
2022-02-03 16:47:29 +0100

    Product rest controller created

--- src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
+++ src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
@@ -1,4 +1,35 @@
 package com.phoenix.phoenix.controllers;
 
-public class HomeController {
+import com.phoenix.phoenix.data.dto.ProductDto;
+import com.phoenix.phoenix.data.models.Product;
+import com.phoenix.phoenix.service.product.ProductService;
+import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.net.ProtocolException;
+import java.net.URI;
+import java.util.List;
+
+public class ProductRestController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<?> findAllProducts(){
+
+        List<Product> productList = productService.getAllProducts();
+
+        return ResponseEntity.ok().body(productList);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(ProductDto productDto){
+
+        return null;
+    }
 }
