0696694dd8f688f33bebac464359ef8d1e3a28e0
Ozioma <oziomaokoroafor@gmail.com>
2022-02-03 16:12:25 +0100

    Exception class

---
+++ src/main/java/com/phoenix/phoenix/web/exceptions/ProductDoesNotExistException.java
@@ -0,0 +1,7 @@
+package com.phoenix.phoenix.web.exceptions;
+
+public class ProductDoesNotExistException extends Exception {
+    public ProductDoesNotExistException(String s) {
+        super(s);
+    }
+}
