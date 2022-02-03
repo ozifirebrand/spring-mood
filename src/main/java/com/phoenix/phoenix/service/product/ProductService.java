ffd8ea22e69268931845e8d77753a6086f716512
Ozioma <oziomaokoroafor@gmail.com>
2022-02-03 17:17:04 +0100

    Duplicate product name exception

--- src/main/java/com/phoenix/phoenix/service/product/ProductService.java
+++ src/main/java/com/phoenix/phoenix/service/product/ProductService.java
@@ -2,6 +2,7 @@ package com.phoenix.phoenix.service.product;
 
 import com.phoenix.phoenix.data.dto.ProductDto;
 import com.phoenix.phoenix.data.models.Product;
+import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
 import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
 
 import java.util.List;
@@ -9,6 +10,6 @@ import java.util.List;
 public interface ProductService {
     List<Product> getAllProducts();
     Product findProductById(Long id) throws ProductDoesNotExistException;
-    Product createProduct(ProductDto productDto);
+    Product createProduct(ProductDto productDto) throws BusinessLogicException;
 
 }
