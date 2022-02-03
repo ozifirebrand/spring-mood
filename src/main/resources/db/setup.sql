67832f88cbaff37e98de32225e317c8cc8543a82
Ozioma <oziomaokoroafor@gmail.com>
2022-02-03 09:46:33 +0100

    Testing application.properties

---
+++ src/main/resources/db/setup.sql
@@ -0,0 +1,7 @@
+create database phoenix_db;
+
+create user 'phoenix_user'@'localhost' identified by "password"''
+
+grant all privileges on phoenix_db.* to 'phoenix_user'@'localhost';
+
+flush privileges;
--- src/test/java/com/phoenix/phoenix/PhoenixApplicationTests.java
+++ src/test/java/com/phoenix/phoenix/PhoenixApplicationTests.java
@@ -1,13 +1,26 @@
 package com.phoenix.phoenix;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
+@Slf4j
 class PhoenixApplicationTests {
 
+	@Value("${test.property.name}")
+	private String testName;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void valueExists(){
+		assertThat(testName).isEqualTo("phoenix");
+		log.info(testName);
 	}
 
 }
