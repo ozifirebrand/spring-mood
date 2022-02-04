f312564a9a9623e85f5c2415398cbd75abc03b6a
Ozioma <oziomaokoroafor@gmail.com>
2022-02-04 16:15:32 +0100

    Update

--- src/main/java/com/phoenix/phoenix/data/repository/ProductRepository.java
+++ src/main/java/com/phoenix/phoenix/data/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.phoenix.phoenix.data.repository;
 
 import com.phoenix.phoenix.data.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findProductByName(String name);
 }
