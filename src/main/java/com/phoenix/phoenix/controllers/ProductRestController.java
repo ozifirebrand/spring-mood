2d4befcfdf362dd1fe21ab9ac80a75a94e93d4a6
Ozioma <oziomaokoroafor@gmail.com>
2022-02-04 09:59:33 +0100

    Product rest controller

--- src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
+++ src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
@@ -4,17 +4,14 @@ import com.phoenix.phoenix.data.dto.ProductDto;
 import com.phoenix.phoenix.data.models.Product;
 import com.phoenix.phoenix.service.product.ProductService;
 import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
-import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.ProtocolException;
-import java.net.URI;
 import java.util.List;
-
+@RestController("api/products")
 public class ProductRestController {
 
     @Autowired
