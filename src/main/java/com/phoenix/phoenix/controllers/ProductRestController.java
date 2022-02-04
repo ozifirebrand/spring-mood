c9cefa647099373ffcc39fdcd1ea851d15cb94ea
Ozioma <oziomaokoroafor@gmail.com>
2022-02-04 10:16:49 +0100

    Product rest controller

--- src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
+++ src/main/java/com/phoenix/phoenix/controllers/ProductRestController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@RestController("api/products")
+@RestController
+@RequestMapping("api/products")
 public class ProductRestController {
 
     @Autowired
@@ -25,7 +27,7 @@ public class ProductRestController {
         return ResponseEntity.ok().body(productList);
     }
 
-    @PostMapping
+    @PostMapping("products")
     public ResponseEntity<?> createProduct(ProductDto productDto){
 
         try{
