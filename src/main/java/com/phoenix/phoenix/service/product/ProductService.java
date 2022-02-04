115f35cbd15bc5bed994e8e2890876cfbd0cc487
Ozioma <oziomaokoroafor@gmail.com>
2022-02-04 12:47:08 +0100

    Update

--- src/main/java/com/phoenix/phoenix/service/product/ProductService.java
+++ src/main/java/com/phoenix/phoenix/service/product/ProductService.java
@@ -1,5 +1,8 @@
 package com.phoenix.phoenix.service.product;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import com.phoenix.phoenix.data.dto.ProductDto;
 import com.phoenix.phoenix.data.models.Product;
 import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
@@ -11,6 +14,6 @@ public interface ProductService {
     List<Product> getAllProducts();
     Product findProductById(Long id) throws ProductDoesNotExistException;
     Product createProduct(ProductDto productDto) throws BusinessLogicException;
-    Product updateProduct(Long productId, ProductDto productDto);
+    Product updateProduct(Long productId, JsonPatch patch) throws BusinessLogicException, JsonPatchException, JsonProcessingException;
 
 }
