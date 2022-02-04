b7dc674d1c6bc7484b2052d5497b71470cd56b7e
Ozioma <oziomaokoroafor@gmail.com>
2022-02-04 19:45:20 +0100

    Test commit

--- src/test/java/com/phoenix/phoenix/controllers/ProductRestControllerTest.java
+++ src/test/java/com/phoenix/phoenix/controllers/ProductRestControllerTest.java
@@ -1,5 +1,7 @@
 package com.phoenix.phoenix.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.phoenix.phoenix.data.models.Product;
 import com.phoenix.phoenix.data.repository.ProductRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,12 +9,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = {"/db/insert.sql"})
 
 class ProductRestControllerTest {
 
@@ -20,25 +32,55 @@ class ProductRestControllerTest {
     private ProductRepository repository;
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper;
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        mapper = new ObjectMapper();
     }
 
     @Test
-    void findAllProducts() {
+    public void findAllProducts() throws Exception {
+         mockMvc.perform(get("/api/product")
+                .contentType("application/json"))
+                .andExpect(status().is(200))
+                .andDo(print());
+
     }
 
     @Test
-    void createProduct() {
+    public void createProduct() throws Exception {
+        Product product = new Product();
+        product.setName("Mymy");
+        product.setPrice(550
+        );
+        product.setDescription("A detergent by Bimbo");
+        product.setQuantity(10);
+        product.setImageUrl("product.detergent.bimbo");
+
+        String requestBody = mapper.writeValueAsString(product);
+        mockMvc.perform(post("/api/product")
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andExpect(status().is(200))
+                .andDo(print());
     }
 
     @Test
-    void updateProduct() {
+    public void updateProduct() throws Exception {
+        Product product = repository.findById(12L).orElse(null);
+        assertThat(product).isNotNull();
+
+        mockMvc.perform(patch("/api/product/12").
+                contentType("application/json-patch+json").
+                content(Files.readAllBytes(Path.of("payload.json")))).
+                andExpect(status().is(200)).
+                andDo(print());
+
+        product = repository.findById(12L).orElse(null);
+        assertThat(product).isNotNull();
+        assertThat(product.getName()).isEqualTo("Okin");
     }
 }
