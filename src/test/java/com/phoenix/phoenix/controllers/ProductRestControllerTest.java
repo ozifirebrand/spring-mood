93ed1dea204cd1f7103c84c8142f197576f94fe1
Ozioma <oziomaokoroafor@gmail.com>
2022-02-04 16:19:35 +0100

    Test restcontroller

---
+++ src/test/java/com/phoenix/phoenix/controllers/ProductRestControllerTest.java
@@ -0,0 +1,44 @@
+package com.phoenix.phoenix.controllers;
+
+import com.phoenix.phoenix.data.repository.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+class ProductRestControllerTest {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findAllProducts() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+}
