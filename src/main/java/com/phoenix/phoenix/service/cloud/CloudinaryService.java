c218644437e5bf74a561825dd2a972c811ed3852
Ozioma <oziomaokoroafor@gmail.com>
2022-02-11 12:17:10 +0100

    Cloudinary service commit

--- src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryService.java
+++ src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryService.java
@@ -1,8 +1,12 @@
 package com.phoenix.phoenix.service.cloud;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public interface CloudinaryService {
-    Map<?, ?> upload(File file, Map<?,?> params);
+    Map<?, ?> upload(File file, Map<?,?> params) throws IOException;
+    Map<?, ?> upload(MultipartFile file, Map<?,?> params) throws IOException;
 }
