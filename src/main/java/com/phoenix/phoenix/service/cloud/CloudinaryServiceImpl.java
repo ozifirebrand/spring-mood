cbb390739e25524b79532ff2b50be6b4dd367072
Ozioma <oziomaokoroafor@gmail.com>
2022-02-11 12:16:42 +0100

    Cloudinary service imp;

--- src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryServiceImpl.java
+++ src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryServiceImpl.java
@@ -1,13 +1,26 @@
 package com.phoenix.phoenix.service.cloud;
 
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Map;
-@Service("cloudinary")
+@Service
 public class CloudinaryServiceImpl implements CloudinaryService{
+    @Autowired
+    private Cloudinary cloudinary;
     @Override
-    public Map<?, ?> upload(File file, Map<?, ?> params) {
-        return null;
+    public Map<?, ?> upload(File file, Map<?, ?> params) throws IOException {
+
+        return cloudinary.uploader().upload(Files.readAllBytes(file.toPath()), params);
+    }
+
+    @Override
+    public Map<?, ?> upload(MultipartFile file, Map<?, ?> params) throws IOException {
+        return cloudinary.uploader().upload(file.getBytes(), params);
     }
 }
