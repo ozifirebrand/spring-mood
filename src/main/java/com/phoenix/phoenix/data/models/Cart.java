b443efb531f286cbbc962078cb1402025a2bb74c
Ozioma <oziomaokoroafor@gmail.com>
2022-02-11 17:16:37 +0100

    Cart commit

--- src/main/java/com/phoenix/phoenix/data/models/Cart.java
+++ src/main/java/com/phoenix/phoenix/data/models/Cart.java
@@ -1,2 +1,20 @@
-package com.phoenix.phoenix.data.models;public class Cart {
+package com.phoenix.phoenix.data.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private LocalDateTime createdAt;
+
+    @Transient
+    private Double totalPrice;
+
 }
