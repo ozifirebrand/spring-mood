bf5fe4a54a7ac3605e45aa351693e5a4c10c0bae
Ozioma <oziomaokoroafor@gmail.com>
2022-02-11 19:37:11 +0100

    Cart commit

--- src/main/java/com/phoenix/phoenix/data/models/Cart.java
+++ src/main/java/com/phoenix/phoenix/data/models/Cart.java
@@ -1,9 +1,11 @@
 package com.phoenix.phoenix.data.models;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,9 +14,22 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @OneToMany(cascade =  CascadeType.PERSIST)
+    private List<Item> itemList;
 
     @Transient
     private Double totalPrice;
+
+
+    public void addItem(Item item){
+        if ( itemList == null ){
+            itemList = new ArrayList<>();
+        }
+        itemList.add(item);
+    }
 
 }
