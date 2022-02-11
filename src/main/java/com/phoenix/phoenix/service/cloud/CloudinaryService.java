01a7604a03e5cca090f36cf67249e91fc308cea5
Ozioma <oziomaokoroafor@gmail.com>
2022-02-11 12:30:01 +0100

    Refactoring cloud service

--- src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryService.java
+++ src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryService.java
@@ -7,6 +7,6 @@ import java.io.IOException;
 import java.util.Map;
 
 public interface CloudinaryService {
-    Map<?, ?> upload(File file, Map<?,?> params) throws IOException;
-    Map<?, ?> upload(MultipartFile file, Map<?,?> params) throws IOException;
+    Map<?, ?> upload(byte [] bytes, Map<?,?> params) throws IOException;
+
 }
