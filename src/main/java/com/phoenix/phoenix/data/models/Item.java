db8ee13970b5df8560d24858bbbb360c4a729d05
Ozioma <oziomaokoroafor@gmail.com>
2022-02-11 17:17:02 +0100

    Item commit

--- src/main/java/com/phoenix/phoenix/data/models/Item.java
+++ src/main/java/com/phoenix/phoenix/data/models/Item.java
@@ -1,2 +1,20 @@
-package com.phoenix.phoenix.data.models;public class Item {
+package com.phoenix.phoenix.data.models;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Item {
+    @Id
+//    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Product product;
+
+    private int quantityAdded;
+
 }
