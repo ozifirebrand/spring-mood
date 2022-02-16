049fca9eb87d105b984324a57ccac2a6d9f5ec5e
Ozioma <oziomaokoroafor@gmail.com>
2022-02-16 12:21:18 +0100

    App user model

--- src/main/java/com/phoenix/phoenix/data/models/AppUser.java
+++ src/main/java/com/phoenix/phoenix/data/models/AppUser.java
@@ -1,2 +1,37 @@
-package com.phoenix.phoenix.data.models;public class AppUser {
+package com.phoenix.phoenix.data.models;
+
+import lombok.Data;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+
+    @Column(length = 500)
+    private String address;
+
+    @Column(unique = true)
+    private String email;
+
+    @OneToOne
+    @Getter
+    private final Cart cart;
+
+    public AppUser(){
+        this.cart = new Cart();
+    }
 }
