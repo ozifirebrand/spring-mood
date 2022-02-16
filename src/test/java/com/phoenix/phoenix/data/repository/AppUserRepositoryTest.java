3f3569e6c6f5d76ecfa74563a8e141448722d96b
Ozioma <oziomaokoroafor@gmail.com>
2022-02-16 12:21:01 +0100

    App user test class

--- src/test/java/com/phoenix/phoenix/data/repository/AppUserRepositoryTest.java
+++ src/test/java/com/phoenix/phoenix/data/repository/AppUserRepositoryTest.java
@@ -1,4 +1,17 @@
+package com.phoenix.phoenix.data.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AppUserRepositoryTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 }
