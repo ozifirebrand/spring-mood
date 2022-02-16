69ca219d5bfe0718a9a562d766240ce6056d8ff6
Ozioma <oziomaokoroafor@gmail.com>
2022-02-16 10:11:05 +0100

    First commit wednesday

--- src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryServiceImpl.java
+++ src/main/java/com/phoenix/phoenix/service/cloud/CloudinaryServiceImpl.java
@@ -3,11 +3,7 @@ package com.phoenix.phoenix.service.cloud;
 import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Map;
 @Service
 public class CloudinaryServiceImpl implements CloudinaryService{
--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -8,4 +8,17 @@ values(12, 'Ariel',554, 3 ),
 (14, 'Klin',554, 3 ),
 (15, 'Sunlight',554, 3 );
 
+
+insert into item(id, product_id, quantity_added_to_cart)
+values(102, 14, 2),
+       (122, 12, 4),
+       (133, 13, 1);
+
+insert into cart(id)
+values(345)
+
+insert into cart_item_list(cart_id, item_list_id)
+values(345, 102),
+  (345, 122),
+  (345, 133);
 set foreign_key_checks = 1;
