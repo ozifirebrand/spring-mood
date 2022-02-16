d73f9f0e10faa1baeedcf74346d158c79d89925d
Ozioma <oziomaokoroafor@gmail.com>
2022-02-16 17:02:31 +0100

    Cart item dto

--- src/main/java/com/phoenix/phoenix/data/dto/CartItemDto.java
+++ src/main/java/com/phoenix/phoenix/data/dto/CartItemDto.java
@@ -1,2 +1,11 @@
-package com.phoenix.phoenix.data.dto;public class CartItemDto {
+package com.phoenix.phoenix.data.dto;
+
+import lombok.Data;
+
+@Data
+public class CartItemDto {
+    private Long productId;
+    private Long userId;
+    private int quantity;
+
 }
