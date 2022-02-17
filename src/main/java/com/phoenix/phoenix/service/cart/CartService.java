03161c14f7825dd7a2e3d2da8bfa842c70ef359e
Ozioma <oziomaokoroafor@gmail.com>
2022-02-17 14:17:57 +0100

    Cart service commit

--- src/main/java/com/phoenix/phoenix/service/cart/CartService.java
+++ src/main/java/com/phoenix/phoenix/service/cart/CartService.java
@@ -1,4 +1,7 @@
 package com.phoenix.phoenix.service.cart;
 
+import com.phoenix.phoenix.data.dto.CartItemDto;
+
 public interface CartService {
+    public void addItemToCart(CartItemDto itemDto);
 }
