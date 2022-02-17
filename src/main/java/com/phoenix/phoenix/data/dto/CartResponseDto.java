cb8301d9cbc46e98d16cfa3bcf761a6b85de7358
Ozioma <oziomaokoroafor@gmail.com>
2022-02-17 15:41:12 +0100

    Cart response dto

--- src/main/java/com/phoenix/phoenix/data/dto/CartResponseDto.java
+++ src/main/java/com/phoenix/phoenix/data/dto/CartResponseDto.java
@@ -1,4 +1,13 @@
 package com.phoenix.phoenix.data.dto;
 
-public class CartRequestDto {
+import com.phoenix.phoenix.data.models.Item;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+@Data
+@Builder
+public class CartResponseDto {
+    private List<Item> itemList;
+    private double totalPrice;
 }
