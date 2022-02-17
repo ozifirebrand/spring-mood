6ceb8465d08a2e3350430dba63d71d5c3bd2c32d
Ozioma <oziomaokoroafor@gmail.com>
2022-02-17 18:34:45 +0100

    Cart controller commit

--- src/main/java/com/phoenix/phoenix/controllers/CartController.java
+++ src/main/java/com/phoenix/phoenix/controllers/CartController.java
@@ -1,2 +1,35 @@
-package com.phoenix.phoenix.controllers;public class CartController {
+package com.phoenix.phoenix.controllers;
+
+import com.phoenix.phoenix.data.dto.CartRequestDto;
+import com.phoenix.phoenix.data.dto.CartResponseDto;
+import com.phoenix.phoenix.service.cart.CartService;
+import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
+import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
+import com.phoenix.phoenix.web.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+
+    @PostMapping
+    public ResponseEntity<?> addItemToCart(@RequestBody CartRequestDto cartRequestDto){
+        CartResponseDto cartResponseDto ;
+        try{
+            cartResponseDto = cartService.addItemToCart(cartRequestDto);
+        } catch (UserNotFoundException | ProductDoesNotExistException | BusinessLogicException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartResponseDto);
+    }
 }
