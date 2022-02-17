5b4de29498a4e0193fb831ae5ed2592b8bd057b2
Ozioma <oziomaokoroafor@gmail.com>
2022-02-17 14:40:23 +0100

    Add item to cart

--- src/main/java/com/phoenix/phoenix/service/cart/CartServiceImpl.java
+++ src/main/java/com/phoenix/phoenix/service/cart/CartServiceImpl.java
@@ -8,8 +8,13 @@ import com.phoenix.phoenix.data.models.Product;
 import com.phoenix.phoenix.data.repository.AppUserRepository;
 import com.phoenix.phoenix.data.repository.CartRepository;
 import com.phoenix.phoenix.data.repository.ProductRepository;
+import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
+import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
+import com.phoenix.phoenix.web.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -26,14 +31,29 @@ public class CartServiceImpl implements CartService{
     private ProductRepository productRepository;
 
     @Override
-    public void addItemToCart(CartItemDto cartItemDto) {
-        AppUser user = appUserRepository.findById(cartItemDto.getUserId()).orElse(null);
+    public void addItemToCart(CartItemDto cartItemDto)
+            throws UserNotFoundException, ProductDoesNotExistException,
+            BusinessLogicException {
+
+        Optional<AppUser> query = appUserRepository.findById(cartItemDto.getUserId());
+
+        if ( query.isEmpty() ){
+            throw new UserNotFoundException("User with ID "+cartItemDto.getUserId()+ " not found");
+        }
+        AppUser user = query.get();
 
         //get user cart
         Cart cart = user.getCart();
 
         //check user exists
         Product product = productRepository.findById(13L).orElse(null);
+        if ( product ==null ){
+            throw new ProductDoesNotExistException("Product with ID "+
+                    cartItemDto.getProductId()+" does not exist");
+        }
+
+        if ( quantityIsNotValid(product, cartItemDto.getQuantity()) )
+            throw new BusinessLogicException("Quantity too large");
 
         //add product to cart
         Item cartItem = new Item(product, cartItemDto.getQuantity());
@@ -42,5 +62,9 @@ public class CartServiceImpl implements CartService{
         //save cart
         cartRepository.save(cart);
 
+    }
+
+    private boolean quantityIsNotValid(Product product, int quantity){
+        return product.getQuantity() <= quantity;
     }
 }
---
+++ src/main/java/com/phoenix/phoenix/web/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.phoenix.phoenix.exceptions;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
