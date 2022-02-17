f32533aa3e029e0d036182c124909851b1289587
Ozioma <oziomaokoroafor@gmail.com>
2022-02-17 14:40:42 +0100

    Add item to cart

--- src/main/java/com/phoenix/phoenix/service/cart/CartService.java
+++ src/main/java/com/phoenix/phoenix/service/cart/CartService.java
@@ -1,7 +1,10 @@
 package com.phoenix.phoenix.service.cart;
 
 import com.phoenix.phoenix.data.dto.CartItemDto;
+import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
+import com.phoenix.phoenix.web.exceptions.ProductDoesNotExistException;
+import com.phoenix.phoenix.web.exceptions.UserNotFoundException;
 
 public interface CartService {
-    public void addItemToCart(CartItemDto itemDto);
+    public void addItemToCart(CartItemDto itemDto) throws UserNotFoundException, ProductDoesNotExistException, BusinessLogicException;
 }
