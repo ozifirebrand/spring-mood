b4fadf584c2f040f134b44892e8fccbc2746eb51
Ozioma <oziomaokoroafor@gmail.com>
2022-02-18 12:16:31 +0100

    Cart service commit

--- src/main/java/com/phoenix/phoenix/service/cart/CartService.java
+++ src/main/java/com/phoenix/phoenix/service/cart/CartService.java
@@ -1,5 +1,6 @@
 package com.phoenix.phoenix.service.cart;
 
+import com.github.fge.jsonpatch.JsonPatch;
 import com.phoenix.phoenix.data.dto.CartRequestDto;
 import com.phoenix.phoenix.data.dto.CartResponseDto;
 import com.phoenix.phoenix.data.models.Cart;
@@ -10,4 +11,5 @@ import com.phoenix.phoenix.web.exceptions.UserNotFoundException;
 public interface CartService {
     CartResponseDto addItemToCart(CartRequestDto cartRequestDto) throws UserNotFoundException, ProductDoesNotExistException, BusinessLogicException;
     Cart viewCart(Long id);
+    CartResponseDto updateCart(Long id, JsonPatch jsonPatch);
 }
