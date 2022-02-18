f736b0415fabea8ac2965173bb8607cadda3601a
Ozioma <oziomaokoroafor@gmail.com>
2022-02-18 17:44:25 +0100

    Cart controller comit

--- src/main/java/com/phoenix/phoenix/web/controllers/CartController.java
+++ src/main/java/com/phoenix/phoenix/web/controllers/CartController.java
@@ -2,15 +2,13 @@ package com.phoenix.phoenix.web.controllers;
 
 import com.phoenix.phoenix.data.dto.CartRequestDto;
 import com.phoenix.phoenix.data.dto.CartResponseDto;
+import com.phoenix.phoenix.data.dto.CartUpdateDto;
 import com.phoenix.phoenix.service.cart.CartService;
 import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -29,5 +27,25 @@ public class CartController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         }
         return ResponseEntity.status(HttpStatus.OK).body(cartResponseDto);
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> updateCartItems(@RequestBody CartUpdateDto updateDto){
+        try{
+            CartResponseDto responseDto = cartService.updateCartItem(updateDto);
+            return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+        } catch (BusinessLogicException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getCartById(@PathVariable("userId") Long id){
+        try{
+            CartResponseDto responseDto = cartService.viewCart(id);
+            return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+        } catch (BusinessLogicException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
     }
 }
