fab95abc8f9126d8960d9295f184a6073c00ad5f
Ozioma <oziomaokoroafor@gmail.com>
2022-02-23 14:08:40 +0100

    App user repository commit

--- src/main/java/com/phoenix/phoenix/data/repository/AppUserRepository.java
+++ src/main/java/com/phoenix/phoenix/data/repository/AppUserRepository.java
@@ -3,6 +3,8 @@ package com.phoenix.phoenix.data.repository;
 import com.phoenix.phoenix.data.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+import java.util.Optional;
 
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByEmail(String email);
 }
