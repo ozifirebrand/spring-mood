30313e46f574eaaa27522bf3d2e289b30aefcd56
Ozioma <oziomaokoroafor@gmail.com>
2022-02-23 16:53:07 +0100

    App user service commit

--- src/main/java/com/phoenix/phoenix/service/user/AppUserService.java
+++ src/main/java/com/phoenix/phoenix/service/user/AppUserService.java
@@ -1,2 +1,10 @@
-package com.phoenix.phoenix.service.user;public class AppUserService {
+package com.phoenix.phoenix.service.user;
+
+import com.phoenix.phoenix.data.dto.AppUserRequestDto;
+import com.phoenix.phoenix.data.dto.AppUserResponseDto;
+import com.phoenix.phoenix.web.exceptions.BusinessLogicException;
+
+public interface AppUserService {
+
+    AppUserResponseDto createUser(AppUserRequestDto userRequestDto) throws BusinessLogicException;
 }
