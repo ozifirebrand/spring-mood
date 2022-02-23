5336ef3fac671b0b98de773c64e36d1023a5df2b
Ozioma <oziomaokoroafor@gmail.com>
2022-02-23 14:19:41 +0100

    Web secirity config

--- src/main/java/com/phoenix/phoenix/security/WebSecurityConfig.java
+++ src/main/java/com/phoenix/phoenix/security/WebSecurityConfig.java
@@ -1,2 +1,19 @@
-package com.phoenix.phoenix.security;public class WebSecurityConfig {
+package com.phoenix.phoenix.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 }
