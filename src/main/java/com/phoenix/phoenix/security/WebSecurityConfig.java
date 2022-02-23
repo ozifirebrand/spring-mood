40af0c692d3083733bdf411e55254ffb0ec4ac71
Ozioma <oziomaokoroafor@gmail.com>
2022-02-23 16:51:31 +0100

    Web sec configuration

--- src/main/java/com/phoenix/phoenix/security/WebSecurityConfig.java
+++ src/main/java/com/phoenix/phoenix/security/WebSecurityConfig.java
@@ -1,10 +1,18 @@
 package com.phoenix.phoenix.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    RDBUserDetailServiceImpl rdbUserDetailService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -13,7 +21,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
+//                .addFilter(new JWTAuthenticationFilter())
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(rdbUserDetailService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+
+    @Bean("passwordEncoder")
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
