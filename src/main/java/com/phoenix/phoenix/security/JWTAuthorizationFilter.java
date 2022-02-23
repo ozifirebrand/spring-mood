c7e8d5fe9a83e9b7ff0a00d6d69178e21534b931
Ozioma <oziomaokoroafor@gmail.com>
2022-02-23 17:40:56 +0100

    JWT Authorization filter commit

--- src/main/java/com/phoenix/phoenix/security/JWTAuthorizationFilter.java
+++ src/main/java/com/phoenix/phoenix/security/JWTAuthorizationFilter.java
@@ -1,2 +1,21 @@
-package com.phoenix.phoenix.security;public class JWTAuthorizationFilter {
+package com.phoenix.phoenix.security;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager){
+        super(authenticationManager);
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain chain){
+
+    }
+
 }
