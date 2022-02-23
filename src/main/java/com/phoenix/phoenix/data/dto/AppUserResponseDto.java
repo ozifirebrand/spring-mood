1ed6821caf6b89aa6abd02f58e3df3a65b63d236
Ozioma <oziomaokoroafor@gmail.com>
2022-02-23 16:51:09 +0100

    App user dto commit

--- src/main/java/com/phoenix/phoenix/data/dto/AppUserResponseDto.java
+++ src/main/java/com/phoenix/phoenix/data/dto/AppUserResponseDto.java
@@ -1,2 +1,15 @@
-package com.phoenix.phoenix.data.dto;public class AppUserResponseDto {
+package com.phoenix.phoenix.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AppUserResponseDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
 }
