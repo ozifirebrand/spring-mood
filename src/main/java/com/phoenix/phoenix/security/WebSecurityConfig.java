cec63889bcc137b3e3c8e572cd6c10f945b4f8eb
Ozioma <oziomaokoroafor@gmail.com>
2022-02-23 17:40:28 +0100

    Web security config

--- src/main/java/com/phoenix/phoenix/security/WebSecurityConfig.java
+++ src/main/java/com/phoenix/phoenix/security/WebSecurityConfig.java
@@ -3,11 +3,14 @@ package com.phoenix.phoenix.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -18,10 +21,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-//                .addFilter(new JWTAuthenticationFilter())
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .exceptionHandling()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
