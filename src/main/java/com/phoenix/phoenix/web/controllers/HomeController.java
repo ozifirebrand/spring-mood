9bb127c550f4a3dc6c88183ed195026a674109b5
Ozioma <oziomaokoroafor@gmail.com>
2022-03-12 21:06:35 +0100

    Home controller

--- src/main/java/com/phoenix/phoenix/web/controllers/HomeController.java
+++ src/main/java/com/phoenix/phoenix/web/controllers/HomeController.java
@@ -1,7 +1,10 @@
 package com.phoenix.phoenix.web.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 
